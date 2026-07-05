7f8aa1c4257620ed26afb51abb61b9accf8ff10d
Nahuel velez <veleznahuel515@gmail.com>
2026-07-05 17:32:38 -0300

    fix de errores en main.js y vite.config.js

--- WorktimeRecord.UI/worktime-record-app/src/main.js
+++ WorktimeRecord.UI/worktime-record-app/src/main.js
@@ -4,7 +4,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
+import router from './routes'
 
 import vuetify from "./plugins/vuetify"
 
--- WorktimeRecord.UI/worktime-record-app/vite.config.js
+++ WorktimeRecord.UI/worktime-record-app/vite.config.js
@@ -16,6 +16,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@modules': fileURLToPath(new URL('./src/modules', import.meta.url)),
     },
   },
 })
