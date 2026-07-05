069c4b52c967a442f9030a9e586a7e761196fe17
Nahuel velez <veleznahuel515@gmail.com>
2026-07-05 17:26:58 -0300

    se hacen rutas para la optimizacion del ancho de banda

---
+++ WorktimeRecord.UI/worktime-record-app/src/routes/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/home_record',
+      name: "home_record",
+      component: () => import('@modules/workRegistration/views/WorkRegistrationView.vue')
+    },
+
+    {
+        path: '/records',
+        name: "records",
+        component: () => import('@modules/workRegistrationTable/views/WorkRegistrationTableView.vue')
+    },
+
+    { path: '/:pathMatch(.*)*', redirect: '/home_record' }
+  ]
+})
+
+export default routes
